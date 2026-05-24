925c3bf629fc869282a9b7f04bc30d1bbb45d8d7
kksand3r <nonamesan9i@gmail.com>
2026-05-24 23:32:50 +0300

    update next..config

--- event-aggregator-ui/next.config.ts
+++ event-aggregator-ui/next.config.ts
@@ -15,7 +15,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://52.236.142.128:8080/api/:path*',
+        destination: 'http://52.236.142.128:5194/api/:path*',
       },
     ];
   },
