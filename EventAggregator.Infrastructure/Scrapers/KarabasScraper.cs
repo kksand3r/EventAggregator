b167247e837d329b56e8a7f5f09539c9c2a7db5d
kksand3r <nonamesan9i@gmail.com>
2026-05-27 15:52:29 +0300

    update (може рабочий)

--- EventAggregator.Infrastructure/Scrapers/KarabasScraper.cs
+++ EventAggregator.Infrastructure/Scrapers/KarabasScraper.cs
@@ -18,10 +18,10 @@ public class KarabasScraper : IEventScraper
 
     private readonly string[] _citySlugs =
     {
-        "kyiv", "odesa", "dnipro", "lviv", "kharkiv", "ivano-frankivsk",
-        "vinnytsia", "poltava", "zhytomyr", "zaporizhzhia", "ternopil",
-        "chernivtsi", "chernihiv", "sumy", "khmelnytskyi", "rivne",
-        "lutsk", "mykolaiv", "uzhhorod", "kropyvnytskyi"
+        "uzhhorod"  //kyiv", "odesa", "dnipro", "lviv", "kharkiv", "ivano-frankivsk",
+        //"vinnytsia", "poltava", "zhytomyr", "zaporizhzhia", "ternopil",
+        //"chernivtsi", "chernihiv", "sumy", "khmelnytskyi", "rivne",
+        //"lutsk", "mykolaiv", "uzhhorod", "kropyvnytskyi"
     };
 
     private readonly string[] _categories =
