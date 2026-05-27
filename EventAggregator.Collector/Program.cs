69c3a05926279d6bb2595d29c227f0ba02c4bc5a
kksand3r <nonamesan9i@gmail.com>
2026-05-27 03:07:17 +0300

    update

--- EventAggregator.Collector/Program.cs
+++ EventAggregator.Collector/Program.cs
@@ -32,7 +32,7 @@ try
 
     builder.Services.AddTransient<ScrapingService>();
 
-    //builder.Services.AddTransient<IEventScraper, KarabasScraper>();
+    builder.Services.AddTransient<IEventScraper, KarabasScraper>();
     builder.Services.AddTransient<IEventScraper, ConcertUaScraper>();
 
     using var host = builder.Build();
