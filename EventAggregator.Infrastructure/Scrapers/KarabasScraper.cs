7c8ef79ba7b6885f61b3c7059bf2bfb1c5988d44
kksand3r <nonamesan9i@gmail.com>
2026-05-29 11:37:44 +0300

    update

--- EventAggregator.Infrastructure/Scrapers/KarabasScraper.cs
+++ EventAggregator.Infrastructure/Scrapers/KarabasScraper.cs
@@ -3,7 +3,6 @@ using System.Text.Json;
 using System.Text.RegularExpressions;
 using EventAggregator.Application.Interfaces;
 using EventAggregator.Domain.Models;
-using EventAggregator.Application.Parsing;
 using Microsoft.Extensions.Logging;
 using PuppeteerSharp;
 
